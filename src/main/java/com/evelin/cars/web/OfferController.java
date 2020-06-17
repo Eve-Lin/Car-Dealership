2301499d34f2f2fc595d1ee50c5affcb00a17031
Eve Lin <evelin.dicheva@gmail.com>
2020-06-17 21:32:10 +0300

    Time to get those fellows (the brands and their models) out

---
+++ src/main/java/com/evelin/cars/web/OfferController.java
@@ -0,0 +1,24 @@
+package com.evelin.cars.web;
+
+import com.evelin.cars.service.OfferService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/offers")
+public class OfferController {
+
+    private final OfferService offerService;
+
+    public OfferController(OfferService offerService) {
+        this.offerService = offerService;
+    }
+
+    @GetMapping("/all")
+    public String getOffers(Model offers){
+     offers.addAttribute("offers", offerService.getOffers());
+     return "all";
+    }
+}
